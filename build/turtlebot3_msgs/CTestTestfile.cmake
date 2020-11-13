edaac428a9e13648dabd1acc374e561ba1da8ce2
Ivan5d <ivan.d557d@gmail.com>
2020-11-13 17:34:38 -0600

    .

---
+++ build/turtlebot3_msgs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ivan5d/vision_sim_ws/src/turtlebot3_msgs
+# Build directory: /home/ivan5d/vision_sim_ws/build/turtlebot3_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
